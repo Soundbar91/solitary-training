11dbe4a83a2e23153a3f01278fa08b2ce7dc2e66
신관규 <soundbar91@gmail.com>
2025-02-01 01:15:07 +0900

    Initial commit

---
+++ src/main/java/com/soundbar91/STP/SolitaryTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.soundbar91.STP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolitaryTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolitaryTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soundbar91/STP/SolitaryTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundbar91.STP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolitaryTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
